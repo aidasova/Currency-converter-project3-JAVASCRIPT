cc3f0cf64f3aa45f526d27901c3e1cfd2a32db18
Aida IdriSova <aida-107@mail.ru>
2021-05-29 11:37:54 +0300

    correct html

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 /*class Converter {
     constructor() {
+        this.uri = 'http://api.exchangeratesapi.io/v1/latest?'
+        this.apiKey = 'a9778773f64b39c4542ec6153740a765'
+        this.data = // записываем результат из getData
         this.saleRub = document.querySelector('.sale-currency')
+        базовая валюта, итоговая валюта - найти какие валюты выбраны
     }
-    getRating() {
+    getCurrencyNames() {
 
     }
-    converAmount() {
-
-    }
-    renderData() {
+    getDataFromHost(базовая валюта, итоговая валюта) {
+        // запрос к серверу и получить ответ и вернуть 
+            }
+    render() {
 
     }
     init() {
-
+        this.getDataFromHost(базовая валюта, итоговая валюта)
+        this.render( ) //вывести на экран
+        this.getCurrencyNames ()
     }
 }
 
 let converter = new Converter();
 converter.init();*/
 
-/*saleRub = document.querySelectorAll('.sale-currency')
+saleRub = document.querySelectorAll('.sale-currency')
 console.log(saleRub)
 flag = true;
 
 saleRub.forEach((element) => {
     element.addEventListener('click', (event) => {
         let color = event.target.style.backgroundColor;
-          //  element.background.color = (flag === true) ? 'white' : '#833AE0' ;
-         if (color === '#833AE0') {
+        color = (flag === true) ? 'white' : '#833AE0' ;
+        /* if (color === '#833AE0') {
             event.target.style.backgroundColor = 'white'
          } else {
             event.target.style.backgroundColor = '#833AE0'
-         }
+         }*/
         });
-});*/
+});
