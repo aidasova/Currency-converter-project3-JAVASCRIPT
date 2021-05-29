3590a162b4c026c7cfcb771d0cbbec7365638bce
Aida IdriSova <aida-107@mail.ru>
2021-05-29 09:58:40 +0300

    html

---
+++ script.js
@@ -0,0 +1,36 @@
+/*class Converter {
+    constructor() {
+        this.saleRub = document.querySelector('.sale-currency')
+    }
+    getRating() {
+
+    }
+    converAmount() {
+
+    }
+    renderData() {
+
+    }
+    init() {
+
+    }
+}
+
+let converter = new Converter();
+converter.init();*/
+
+/*saleRub = document.querySelectorAll('.sale-currency')
+console.log(saleRub)
+flag = true;
+
+saleRub.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        let color = event.target.style.backgroundColor;
+          //  element.background.color = (flag === true) ? 'white' : '#833AE0' ;
+         if (color === '#833AE0') {
+            event.target.style.backgroundColor = 'white'
+         } else {
+            event.target.style.backgroundColor = '#833AE0'
+         }
+        });
+});*/
